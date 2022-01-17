918e993cdf7275d780f58738dda7bfec31bd159d
rihemebh <rihemebenhassan@insat.u-carthage.tn>
2022-01-17 17:37:25 +0100

    mail

--- .history/phase1/authentification_20220117162714.py
+++ .history/phase1/authentification_20220117162714.py
@@ -2,7 +2,6 @@ import getpass
 import re
 from phase1.userService import UserService
 
-
 def sign_up(userService: UserService):
     verif = False
     email = None
@@ -11,12 +10,12 @@ def sign_up(userService: UserService):
     last_name = None
     while not verif:
         email = str(input("Enter your email: "))
-        password = str(input("Enter your password: "))
-        password_confirmation = str(input("confirm your password : "))
-        # password = getpass.getpass("Enter your password: ")
-        # password_confirmation = getpass.getpass("Confirm your password: ")
+        # first_name = str(input("Enter your first name: "))
+        # last_name = str(input("Enter your last name: "))
+        password = getpass.getpass("Enter your password: ")
+        password_confirmation = getpass.getpass("Confirm your password: ")
         email_regex = r"\b[A-Za-z0-9]+\.[A-Za-z0-9]+@insat.ucar.tn\b"
-        if (re.fullmatch(email_regex, email) and password == password_confirmation and len(password) > 6):
+        if (re.fullmatch(email_regex, email) and password == password_confirmation and len(password) > 6 ):
             verif = True
     first_name, last_name = email.split("@")[0].split(".")
     try:
@@ -25,17 +24,14 @@ def sign_up(userService: UserService):
     except:
         print("This user already exists, try another user!")
         sign_up(userService=userService)
-
-
-
-
+def generate_random():
+    
 def login(userService: UserService):
     email = None
     password = None
     while email is None and password is None:
         email = str(input("Enter your email: "))
-        password = str(input("Enter your password: "))
-        # password = getpass.getpass("Enter your password: ")
+        password = getpass.getpass("Enter your password: ")
 
     loggedIn = userService.login(email=email, password=password)
     if loggedIn is True:
--- phase1/database_connection.py
+++ phase1/database_connection.py
@@ -1,12 +1,15 @@
 import mysql.connector
 
-class DatabaseConnection: 
+
+class DatabaseConnection:
     db_connection = None
+
     def __init__(self, hostname, user, password, database):
         try:
-            self.db_connection = mysql.connector.connect(host=hostname, user=user, password=password, database=database)            
+            self.db_connection = mysql.connector.connect(host=hostname, user=user, password=password, database=database)
             print("Connected to Database successfully")
         except mysql.connector.Error as error:
             print("Error Database Connection")
+
     def __str__(self):
         return "Database connection object"
