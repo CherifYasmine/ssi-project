8c922d825782b53f7ebb65c5ee939659006f4f21
rihemebh <rihemebenhassan@insat.u-carthage.tn>
2022-01-20 09:57:23 +0100

    chatroom

--- main.py
+++ main.py
@@ -40,27 +40,27 @@ if __name__ == "__main__":
             authentification.login(userService=userService)
         elif choice == 3:
             choice_menu = int(input(
-                    "Entrer votre choix: \n"
-                    "1. Codage et décodage\n"
-                    "   1- Codage \n"
-                    "   2- Decodage \n"
-                    "2. Hashage d'un message \n"
-                    "   1- MD5 \n"
-                    "   2- SHA1 \n"
-                    "   3- SHA256 \n"
-                    "3. Craquage d'un message haché \n"
-                    "   1- MD5 \n"
-                    "   2- SHA1 \n"
-                    "   3- SHA256 \n"
-                    "4. Chiffrement et déchiffrement symétrique d'un message \n"
-                    "   1- DES \n"
-                    "   2- AES256 \n"
-                    "5. Chiffrement et déchiffrement asymétrique d'un message \n"
-                    "   1- RSA \n"
-                    "   2- elgamal \n"
-                    "6. Communication sécurisé entre deux clients (ChatRoom)\n"
-                    "7. Quitter \n"
-                ))
+                "Entrer votre choix: \n"
+                "1. Codage et décodage\n"
+                "   1- Codage \n"
+                "   2- Decodage \n"
+                "2. Hashage d'un message \n"
+                "   1- MD5 \n"
+                "   2- SHA1 \n"
+                "   3- SHA256 \n"
+                "3. Craquage d'un message haché \n"
+                "   1- MD5 \n"
+                "   2- SHA1 \n"
+                "   3- SHA256 \n"
+                "4. Chiffrement et déchiffrement symétrique d'un message \n"
+                "   1- DES \n"
+                "   2- AES256 \n"
+                "5. Chiffrement et déchiffrement asymétrique d'un message \n"
+                "   1- RSA \n"
+                "   2- elgamal \n"
+                "6. Communication sécurisé entre deux clients (ChatRoom)\n"
+                "7. Quitter \n"
+            ))
             if choice_menu == 11:
                 message = input(" entrer votre message ")
                 encode_decode.encode(message)
@@ -87,7 +87,7 @@ if __name__ == "__main__":
                     crypt.des_decrypt()
             if choice_menu == 42:
                 choice_crypt = int(input("1. chiffrement \n" "2. Dechiffrement \n"))
-                if choice_crypt ==1:
+                if choice_crypt == 1:
                     crypt.aes256_crypt()
                 if choice_crypt == 2:
                     crypt.aes256_decrypt()
@@ -109,7 +109,3 @@ if __name__ == "__main__":
 
         elif choice == 4:
             exit()
-
-
-
-
